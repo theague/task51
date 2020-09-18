6b2bbd9e81ca326c866e41bcf7c59753ef3cbbf3
papaponmx <jaime.rios.dev@outlook.com>
2020-09-18 16:34:04 -0500

    Comments from Part 6

--- js/main.js
+++ js/main.js
@@ -3,15 +3,15 @@
  * 1.Get all the cards within container
  * 2. Create a toggle functionality
  *    By toggle I mean to hide one card and display the other
- * 3. Add the controllers logic
- *     what happens when the current item is the next element of an Array
- *
- * 4. Set up the controllers in place
+ * 3. Setup controllers in place
+ *  3.1 Select controllers from the dom
+ *  3.2 Add event listeners
+ * 4. Improve controllers logic
  *
  * */
 
 
-let initialIndex = 0;
+let currentIndex = 0;
 
 // 1.Get all the cards within container
 const cardElements = document.querySelector('#bottom').children;
@@ -19,22 +19,29 @@ const cardElements = document.querySelector('#bottom').children;
 
 // 2. Create toggle functionality
 const showCard = () => {
-    // Hide old card
-    cardElements[initialIndex].style.display = 'none';
+    if (currentIndex === cardElements.length - 1) {
+        cardElements[currentIndex].style.display = 'none';
+        cardElements[0].style.display = 'block';
+        currentIndex = 0;
+        return;
+    }
 
-    ++initialIndex;
-    // Show new card
-    cardElements[initialIndex].style.display = 'block';
-
-    console.log('⚽️');
+    cardElements[currentIndex].style.display = 'none';
+    cardElements[currentIndex + 1].style.display = 'block';
+    ++currentIndex;
 }
 
 
-// 3. Add Controllers logic
+// 3. Setup controllers in place
+    // 3.1 Select controllers from the dom
+    // 3.2 Add event listeners
+    document.querySelector('.previous').onclick = () => showCard();
+    document.querySelector('.next').onclick = () => showCard();
 
 
-// 4. Setup controllers in place
 
-cardElements[0].style.display = 'block';
+// 4. Improve controllers logic
+
+
 // showCard();
 // console.log('🤓');

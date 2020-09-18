742da929aec7eca142679e5adf5a3d21a2f08859
papaponmx <jaime.rios.dev@outlook.com>
2020-09-18 16:53:08 -0500

    Comments from part 7

--- js/main.js
+++ js/main.js
@@ -7,7 +7,8 @@
  *  3.1 Select controllers from the dom
  *  3.2 Add event listeners
  * 4. Improve controllers logic
- *
+ * TODO: Fix the image not being updated.
+ * TODO: Implement ESlint int this repo.
  * */
 
 
@@ -18,7 +19,23 @@ const cardElements = document.querySelector('#bottom').children;
 
 
 // 2. Create toggle functionality
-const showCard = () => {
+const showCard = (showPrevious = false) => {
+    if (showPrevious) {
+        if( currentIndex === 0){
+            // If so, hide first item and show the last one
+            cardElements[0].style.display = 'none';
+            currentIndex = cardElements.length - 1;
+            cardElements[currentIndex].style.display = 'block';
+            return
+        }
+
+        cardElements[currentIndex].style.display = 'none';
+        cardElements[currentIndex - 1].style.display = 'block';
+        --currentIndex;
+        return
+    }
+
+
     if (currentIndex === cardElements.length - 1) {
         cardElements[currentIndex].style.display = 'none';
         cardElements[0].style.display = 'block';
@@ -35,13 +52,5 @@ const showCard = () => {
 // 3. Setup controllers in place
     // 3.1 Select controllers from the dom
     // 3.2 Add event listeners
-    document.querySelector('.previous').onclick = () => showCard();
+    document.querySelector('.previous').onclick = () => showCard(true);
     document.querySelector('.next').onclick = () => showCard();
-
-
-
-// 4. Improve controllers logic
-
-
-// showCard();
-// console.log('🤓');
